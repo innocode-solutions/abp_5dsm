1c9d58ebbf4569964baa1f3ca63cbcf698b655b5
fonsecaigor <fonsecaigr@gmail.com>
2025-10-06 17:04:29 -0300

    AM-011: Modelo de evasão integrado ao endpoint /predict/dropout

--- ai_model/src/normalized/datasetEvasionProcess.py
+++ ai_model/src/normalized/datasetEvasionProcess.py
@@ -5,7 +5,6 @@
 #==============================================================================
 
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
